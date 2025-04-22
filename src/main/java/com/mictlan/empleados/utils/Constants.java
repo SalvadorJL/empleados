f42d9f1fdfc62afe00be6900ab9c3cce493bfe2e
Salvador López <fenix_black168@hotmail.com>
2025-04-22 13:19:57 -0600

    Data is not used

--- src/main/java/com/mictlan/empleados/utils/Constants.java
+++ src/main/java/com/mictlan/empleados/utils/Constants.java
@@ -3,5 +3,4 @@ package com.mictlan.empleados.utils;
 public class Constants {
     public static final String EMPLOYEE_NOT_FOUND = "Employee not found with id: ";
     public static final String API_BASE_PATH = "/api/v1/employees";
-    public static final String DATE_PATTERN = "dd-MM-yyyy";
 }
